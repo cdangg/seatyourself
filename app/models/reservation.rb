e390ab3b0f6d2dac1fb97ba12e6b0c75b6ef8dc7
Celia Dang <dang.celia@gmail.com>
2014-02-09 12:25:15 -0500

    Created a link to My Profile in application view

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,5 @@ class Reservation < ActiveRecord::Base
 	has_many :categories
 	belongs_to :restaurant
 	belongs_to :customer
-	validates_numericality_of :capacity, :less_than => 21
+	validates_numericality_of :num_of_customers, :greater_than => 2, :less_than => 21
 end
